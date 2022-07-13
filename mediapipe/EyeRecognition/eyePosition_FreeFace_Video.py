b60520c927879ea8c8852379a0f44ec63afff3d7
Riccardo Gobbato <100858226+RicGobs@users.noreply.github.com>
2022-07-13 02:58:14 +0200

    Update eyePosition_FreeFace_Video.py

--- mediapipe/EyeRecognition/eyePosition_FreeFace_Video.py
+++ mediapipe/EyeRecognition/eyePosition_FreeFace_Video.py
@@ -5,6 +5,15 @@ import math
  
 mp_face_mesh = mp.solutions.face_mesh
 
+
+
+#
+#
+# POSSIBLE IMPROVEMENT -> CONTROL THE DIRECTION OF FACE WITH VECTOR Z OF MEDIAPIPE (and not geometry of faces)
+#
+#
+
+
 LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
 RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
 
